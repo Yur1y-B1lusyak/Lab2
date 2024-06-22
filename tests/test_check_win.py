afc661d5b1b67edc7f42ce34c31e4b76bf139277
Yuriy <yuriy_bilusyak@knu.ua>
2024-06-22 23:46:44 +0300

    Added report.html

--- tests/test_check_win.py
+++ tests/test_check_win.py
@@ -1,22 +1,23 @@
+import unittest
 from main import check_win
 
+class TestMinesweeper(unittest.TestCase):
+    def test_check_win(self):
+        grid = [
+            [0, 1, -1],
+            [0, 1, 1],
+            [0, 0, 0]
+        ]
+        revealed = [
+            [True, True, False],
+            [True, True, True],
+            [True, True, True]
+        ]
+        self.assertTrue(check_win(grid, revealed))
 
-def test_check_win(self):
-    grid = [
-        [0, 1, -1],
-        [0, 1, 1],
-        [0, 0, 0]
-    ]
-    revealed = [
-        [True, True, False],
-        [True, True, True],
-        [True, True, True]
-    ]
-    self.assertTrue(check_win(grid, revealed))
-
-    revealed = [
-        [True, True, False],
-        [True, True, False],
-        [True, True, True]
-    ]
-    self.assertFalse(check_win(grid, revealed))
+        revealed = [
+            [True, True, False],
+            [True, True, False],
+            [True, True, True]
+        ]
+        self.assertFalse(check_win(grid, revealed))
--- tests/test_create_grid.py
+++ tests/test_create_grid.py
@@ -1,9 +1,10 @@
+import unittest
 from main import create_grid
 
-
-def test_create_grid(self):
-    rows, cols, mine_count = 5, 5, 5
-    grid, mines = create_grid(rows, cols, mine_count)
-    self.assertEqual(len(mines), mine_count)
-    mine_cells = sum(row.count(-1) for row in grid)
-    self.assertEqual(mine_cells, mine_count)
+class TestMinesweeper(unittest.TestCase):
+    def test_create_grid(self):
+        rows, cols, mine_count = 5, 5, 5
+        grid, mines = create_grid(rows, cols, mine_count)
+        self.assertEqual(len(mines), mine_count)
+        mine_cells = sum(row.count(-1) for row in grid)
+        self.assertEqual(mine_cells, mine_count)
